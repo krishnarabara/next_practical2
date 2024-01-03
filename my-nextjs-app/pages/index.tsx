4dead4d924b4450a537295b9b0ff842e813f6f1c
krishnarabara <krishna.rabara@codezeos.com>
2024-01-03 16:06:38 +0530

    change css

--- my-nextjs-app/pages/index.tsx
+++ my-nextjs-app/pages/index.tsx
@@ -1,50 +1,6 @@
-// import React from 'react';
-// import ProductComponent from '../components/ProductComponent';
-// import { Product } from '../interfaces';
-
-// const products: Product [] = [
-//   { id: 1, 
-//     name: 'mobile', 
-//     description: 'iphone'
-//    },
-//   { id: 2,
-//     name: 'tv', 
-//     description: 'sony' 
-//   },
-//   { id: 3, 
-//     name: 'computer', 
-//     description: 'dell' 
-//   },
-//   { id: 4, 
-//     name: 'ac', 
-//     description: 'lg' 
-//   },
-//   { id: 5, 
-//     name: 'freeze', 
-//     description: 'samsung' 
-//   },
-  
-// ];
-
-// const HomePage: React.FC = () => {
-//   return (
-//     <div className="container mx-auto p-4">
-//       <h1 className="text-3xl font-bold mb-4">Product List</h1>
-//       {products.map((product) => (
-//         <ProductComponent key={product.id} product={product} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
-
-
 import React from 'react';
-import ProductComponent from '../components/ProductComponent';
-import { Product } from '../interfaces';
 import { useCart } from '../context/CartContext'; // Import the useCart hook
+import { Product } from '../interfaces';
 
 const products: Product[] = [
   { id: 1, name: 'Mobile', description: 'iPhone' },
@@ -52,15 +8,17 @@ const products: Product[] = [
   { id: 3, name: 'Computer', description: 'Dell' },
   { id: 4, name: 'AC', description: 'LG' },
   { id: 5, name: 'Freeze', description: 'Samsung' },
-];
+  { id: 6, name: 'Mobile', description: 'Android' },
+]
 
 const HomePage: React.FC = () => {
-  const { addToCart } = useCart(); // Use the useCart hook to get addToCart function
+  const { addToCart } = useCart(); 
+  
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Product List</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="bg-blue-500 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
           <div key={product.id} className="border p-4 rounded-md bg-white shadow-md">
             <h2 className="text-lg font-semibold mb-2">{product.name}</h2>
@@ -79,5 +37,4 @@ const HomePage: React.FC = () => {
 };
 
 export default HomePage;
-
 
